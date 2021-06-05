06d8adf5db829dd486fefe380d5ee464a5e83add
JustaveD <yeumytoan0101@gmail.com>
2021-06-05 11:45:39 +0700

    Redo slide icons

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-const icons = document.querySelectorAll('.section-1-icons i');
-
+const icons = document.querySelectorAll(".section-1-icons i");
 let counter = 1;
+const time = 4000;
 
-setInterval(()=>{
+const removeAndAddClassChange = () => {
+  counter++;
+  const icon = document.querySelector(".section-1-icons i.change");
 
-    counter++;
-    const icon = document.querySelector('.section-1-icons i.change');
-    icon.classList.remove('change');
+  icon.classList.remove("change");
+  if (counter > icons.length) {
+    icons[0].classList.add("change");
+    counter = 1;
+  } else {
+    icon.nextElementSibling.classList.add("change");
+  }
+};
 
-    if(counter > icons.length){
-        icons[0].classList.add('change');
-        counter = 1;
-    }
-    else{
-        icon.nextElementSibling.classList.add('change');
-    }
-},4000)
+setInterval(removeAndAddClassChange, time);
