29e18d975144ceaa5a802acd5c1cbd5e6a3dcc48
JustaveD <yeumytoan0101@gmail.com>
2021-06-05 14:44:27 +0700

    Redo section 1 and section 2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-const icons = document.querySelectorAll(".section-1-icons i");
+const icons = document.querySelectorAll('.section-1-icons i');
 let counter = 1;
 const time = 4000;
 
-const removeAndAddClassChange = () => {
+setInterval(()=>{
+
   counter++;
-  const icon = document.querySelector(".section-1-icons i.change");
+  const icon = document.querySelector('.section-1-icons i.change');
 
-  icon.classList.remove("change");
-  if (counter > icons.length) {
-    icons[0].classList.add("change");
+  icon.classList.remove('change');
+  if(counter > icons.length){
+    icons[0].classList.add('change');
     counter = 1;
-  } else {
-    icon.nextElementSibling.classList.add("change");
   }
-};
-
-setInterval(removeAndAddClassChange, time);
+  else{
+    icon.nextElementSibling.classList.add('change');
+  }
+},time)
